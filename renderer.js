d1e5f1ee29f190fd96fe62b08e7f167e3e8077d9
ocboogie <boogie@mikulec.com>
2016-11-12 18:10:40 -0800

    first commit

---
+++ renderer.js
@@ -0,0 +1,18 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const fs = require('fs');
+const {ipcRenderer} = require('electron');
+const jsonlint = require("jsonlint");
+
+let file = './config.json';
+
+try {
+    let fileContent = fs.readFileSync(file, 'utf8');
+    config = jsonlint.parse(fileContent);
+} catch (e) {
+    if (!e.message.startsWith("ENOENT: no such file or directory")) {
+        ipcRenderer.send("jsonError", e.toString());
+        // ipcRenderer.send("jsonError", e.name + ': ' + e.message);
+    }
+}
